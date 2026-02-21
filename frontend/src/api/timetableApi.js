4aaa48322933c5698c03b04f0e2454ae39c3048a
Vivek Shenoy <vivekshenoy6763@gmail.com>
2026-02-21 23:56:12 +0530

    made the api constants for timetabel generation

--- frontend/src/api/timetableApi.js
+++ frontend/src/api/timetableApi.js
@@ -1,36 +1,32 @@
 import api from './axiosInstance';
 
 export const timetableApi = {
-  // --- Class Methods ---
   getClasses: () => api.get('/classes'),
   createClass: (data) => api.post('/classes', data),
   updateClass: (id, data) => api.put(`/classes/${id}`, data),
   deleteClass: (id) => api.delete(`/classes/${id}`),
-
-  // --- Course Methods ---
   getCourses: () => api.get('/courses'),
   getCourseById: (id) => api.get(`/courses/${id}`),
   createCourse: (data) => api.post('/courses', data),
   updateCourse: (id, data) => api.put(`/courses/${id}`, data),
   deleteCourse: (id) => api.delete(`/courses/${id}`),
-
-  // --- Faculty Methods ---
   getFaculty: () => api.get('/faculty'),
   createFaculty: (data) => api.post('/faculty', data),
   updateFaculty: (id, data) => api.put(`/faculty/${id}`, data),
   deleteFaculty: (id) => api.delete(`/faculty/${id}`),
-
-  // --- Room Methods ---
   getRooms: () => api.get('/rooms'),
   createRoom: (data) => api.post('/rooms', data),
   updateRoom: (id, data) => api.put(`/rooms/${id}`, data),
   deleteRoom: (id) => api.delete(`/rooms/${id}`),
-
-  getMetadata: () => api.get('/metadata'),
-  upsertMetadata: (data) => api.post('/metadata', data),
+  createMetadata: (data) => api.post("/metadata", data),
+  getMetadata: () => api.get("/metadata"),
+  updateMetadata: (id, data) => api.put(`/metadata/${id}`, data),
   deleteMetadata: (id) => api.delete(`/metadata/${id}`),
-getRooms: () => api.get('/rooms'),
+  getRooms: () => api.get('/rooms'),
   createRoom: (data) => api.post('/rooms', data),
   updateRoom: (id, data) => api.put(`/rooms/${id}`, data),
   deleteRoom: (id) => api.delete(`/rooms/${id}`),
+  generateTimetable: (classId, suggestions) => api.post('/timetable/generate', { classId, suggestions }),
+  searchTimetables: (params) => api.get('/timetable', { params }),
+  deleteTimetable: (id) => api.delete(`/timetable/${id}`),
 };
